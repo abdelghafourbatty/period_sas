78cb0543d2b7c8d51d05e1d53cc32a8668d69ea1
abdelghafour batty <abdlghafourbatty15@gmail.com>
2022-09-07 16:16:57 +0100

    add ex1

---
+++ ex1.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+ int main()
+ {
+     char nom [127], prenom[127], sexe [127];
+     int age , number ;
+     char prenom1;
+     printf(" enter your name : \n");
+     scanf("%s", &nom);
+     printf("enter your last name : \n");
+     scanf("%c",&prenom1);
+     scanf("%[^\n]" ,prenom);
+
+     printf("enter your sexe : \n");
+     scanf("%s", &sexe);
+     printf("enter your age : \n");
+     scanf("%d", &age);
+     printf("enter your number : \n");
+     scanf("%d" , &number);
+     printf(" nom : %s", nom);
+     printf("prenom : %s" ,prenom);
+
+
+    printf("sexe : %s" , &sexe);
+    printf("age : %d" ,age);
+    printf("number : %d" ,number);
+    return 0;
+ }
