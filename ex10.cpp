766c32767351c1492b9b80b1ba0556249111016d
abdelghafour batty <abdlghafourbatty15@gmail.com>
2022-09-08 09:42:52 +0100

    ex10

---
+++ ex10.cpp
@@ -0,0 +1,12 @@
+#include<stdlib.h>
+#include<stdio.h>
+ 
+int main ( )
+{
+    float r , circumference ;
+    printf ( " enter the radius of the circle : " ) ; scanf ( "%f" , &r ) ;
+    circumference = 2 * 3.14 * r ;
+    printf ( " the circumference of the circle : %f" , circumference ) ;
+    return 0 ;
+}
+
