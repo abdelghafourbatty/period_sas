0f2c46ccc10011fa2863e0240c4b2edf69fbdc07
abdelghafour batty <abdlghafourbatty15@gmail.com>
2022-09-08 10:18:43 +0100

    ex12

---
+++ ex12.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ( )
+{
+    char num [ 3 ] ;
+    printf ( " enter three numbers : " ) ; scanf ( "%s" , &num ) ;
+    printf ( " the revers : %c %c %c " , num [ 2 ] , num [ 1 ] , num [ 0 ] ) ;
+    return 0 ;
+}
+
