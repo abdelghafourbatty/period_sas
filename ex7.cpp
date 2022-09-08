0a0097b83238d1851a1e7ac509658c868bfc3e01
abdelghafour batty <abdlghafourbatty15@gmail.com>
2022-09-08 09:15:17 +0100

    ex7

---
+++ ex7.cpp
@@ -0,0 +1,14 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+ int main()
+{
+int a,b;
+printf("Enter a : "); scanf("%d",&a) ;
+printf("Enter b: "); scanf ("%d",&b) ;
+printf ("\n a+b %d", (a+b) ) ;
+printf ("\n a-b = %d", (a-b) );
+printf ("\n a/b = %.2f", (float) a/ (float) b);
+printf ("\n a%b = %d", (a%b) ) ;
+return 0 ; 
+}
